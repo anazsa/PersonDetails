43ef4b3ce32cb445ac27825c6bec5482407fc3c6
Anazsa Blanding <anazsabee@Anazsas-MacBook-Air.local>
2019-07-30 10:24:29 -0400

    Done

--- src/main/java/hbcu/stay/ready/PersonHandler.java
+++ src/main/java/hbcu/stay/ready/PersonHandler.java
@@ -9,19 +9,28 @@ public class PersonHandler {
     }
 
     public String whileLoop() {
+
         String result = "";
+        int count = 0;
         // create a `counter`
+
+        while (count < personArray.length) {
+           Person identify = personArray[count];
+            count++;
+           String x = String.valueOf(identify);
+           result +=x;
+        }
+
         // while `counter` is less than length of array
-            // begin loop
+        // begin loop
 
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
+        // use `counter` to identify the `current Person` in the array
+        // get `string Representation` of `currentPerson`
+        // append `stringRepresentation` to `result` variable
 
-            // end loop
+        // end loop
         return result;
     }
-
 
 
     public String forLoop() {
@@ -29,6 +38,11 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
+        for ( int count = 0; count < personArray.length; count ++){
+            Person identify = personArray[count];
+            String y = String.valueOf(identify);
+            result += y;
+        }
 
         // use the above clauses to declare for-loop signature
             // begin loop
@@ -46,6 +60,10 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
+        for ( Person count : personArray) {
+            String x = String.valueOf(count);
+            result += x;
+        }
 
         // use the above discoveries to declare for-each-loop signature
             // begin loop
